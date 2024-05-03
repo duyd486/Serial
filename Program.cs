83e48ee5af198fb75195089e44cb87e02cdfb251
duyd486 <duyd486@gmail.com>
2024-05-04 02:28:43 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Nguyen";
+        StoreString(name);
+        Console.WriteLine(ReadString);
+    }
+    static void StoreString(string linkvalue) {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/" + "Link.txt";
+        FileStream fs = new FileStream(path, FileMode.Create);
+        StreamWriter streamWriter= new StreamWriter(fs, System.Text.Encoding.UTF8);
+        streamWriter.Write(linkvalue);
+        streamWriter.Flush();
+        fs.Close();
+
+    }
+    static String ReadString() {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/" + "Link.txt";
+        FileStream fs = new FileStream(path, FileMode.Open);
+        StreamReader sr = new StreamReader(fs, System.Text.Encoding.UTF8);
+        string giatri = sr.ReadToEnd();
+        sr.Close();
+        return giatri;
+    }
+}
